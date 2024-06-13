748824297215bb09ccb22bb57d9fe947bf9f7c24
masubani <masuabni026@gmail.comgit config --global user.email masuabni026@gmail.com>
2024-06-13 14:31:54 +0530

    finished project

--- myform/src/Components/Form.jsx
+++ myform/src/Components/Form.jsx
@@ -1,47 +1,100 @@
-import React from "react";
-import './Form.css'
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
+import React, { useState } from "react";
+import './Form.css';
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
 
-const form = () => {
+const Form = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
 
+    const [errors, setErrors] = useState({});
 
+    const validate = () => {
+        const errors = {};
+        if (!formData.name) {
+            errors.name = 'Name is required';
+        } else if ((/^\d/.test(formData.name))) {
+            errors.name = 'Name cannot start with a number';
+        }
+        if (!formData.email) {
+            errors.email = 'Email is required';
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            errors.email = 'Email is invalid';
+        }
+        if (!formData.password) {
+            errors.password = 'Password is required';
+        } else if (formData.password.length < 6) {
+            errors.password = 'Password must be at least 6 characters long';
+        }
+        return errors;
+    };
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const validationErrors = validate();
+        if (Object.keys(validationErrors).length === 0) {
+            alert('Form submitted successfully:', formData);
+            // Handle form submission (e.g., send data to the server)
+        } else {
+            setErrors(validationErrors);
+        }
+    };
 
-
-
-
-
-
-    
     return (
         <div className="container">
             <div className="header">
                 <div className="text">Form</div>
                 <div className="underline"></div>
             </div>
-            <div className="inputs">
+            <form onSubmit={handleSubmit} className="inputs">
                 <div className="input">
-                    <img src={user_icon} alt="" />
-                    <input type="text" placeholder="Name" />
+                    <img src={user_icon} alt="User Icon" />
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                    {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
                 </div>
                 <div className="input">
-                    <img src={email_icon} alt="" />
-                    <input type="email" placeholder="Email" />
+                    <img src={email_icon} alt="Email Icon" />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
                 </div>
                 <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder="Password"/>
+                    <img src={password_icon} alt="Password Icon" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
                 </div>
-            </div>
-
-            <div className="submit-container">
-                <div className="submit">Submit</div>
-            </div>
+                <div className="submit-container">
+                    <button type="submit" className="submit">Submit</button>
+                </div>
+            </form>
         </div>
-    )
+    );
 }
 
-export default form
+export default Form;
